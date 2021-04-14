c99c3e33296f64a3aa6cf234c38f475e80dee5ad
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-04-14 23:44:26 +0530

    Sample function calls added

--- assets/js/my-script.js
+++ assets/js/my-script.js
@@ -108,28 +108,199 @@ function playAudio(txt) {
 // Function calls
 var functionsList = new Array();
 
+// functionsList.push(() => {
+//   insertNameArrowRightBubble("You", "Hi..", "tick");
+// });
+// functionsList.push(() => {
+//   insertRightBubble("Who are you ?", "tick");
+// });
+// functionsList.push(() => {
+//   insertNameArrowLeftBubble("Rahif", "Hi..", "coming");
+// });
+// functionsList.push(() => {
+//   insertLeftBubble("Who are you ?", "coming");
+// });
+
 functionsList.push(() => {
   insertNameArrowRightBubble("You", "Hi..", "tick");
 });
 functionsList.push(() => {
-  insertRightBubble("Who are you ?", "tick");
+  insertNameArrowLeftBubble("Muhammed Rahif", "Hello..", "coming");
 });
 functionsList.push(() => {
-  insertNameArrowLeftBubble("Rahif", "Hi..", "coming");
+  insertNameArrowRightBubble("You", "What's you name ?", "tick");
 });
 functionsList.push(() => {
-  insertLeftBubble("Who are you ?", "coming");
+  insertNameArrowLeftBubble("Muhammed Rahif", "My name is Rahif!", "coming");
+});
+functionsList.push(() => {
+  insertNameArrowRightBubble(
+    "You",
+    "Rahif, that's nice... Unique name..!",
+    "tick"
+  );
+});
+functionsList.push(() => {
+  insertNameArrowLeftBubble("Muhammed Rahif", "Thanks...", "coming");
+});
+functionsList.push(() => {
+  insertNameArrowRightBubble("You", "Where are you from ?", "tick");
+});
+functionsList.push(() => {
+  insertNameArrowLeftBubble("Muhammed Rahif", "I am from India...", "coming");
+});
+functionsList.push(() => {
+  insertNameArrowRightBubble("You", "Where in India ?", "tick");
+});
+functionsList.push(() => {
+  insertNameArrowLeftBubble("Muhammed Rahif", "Kerala, Malappuram ", "coming");
+});
+functionsList.push(() => {
+  insertNameArrowRightBubble("You", "Hm, what are you doing ?", "tick");
+});
+functionsList.push(() => {
+  insertNameArrowLeftBubble(
+    "Muhammed Rahif",
+    "I am a Software Developer",
+    "coming"
+  );
+});
+functionsList.push(() => {
+  insertNameArrowRightBubble("You", "Good...How can you help me ?", "tick");
+});
+functionsList.push(() => {
+  insertNameArrowLeftBubble(
+    "Muhammed Rahif",
+    "I am web developer... I will create websites for you... Static websites and dynamic websites... If ypu have a business or something like that to market, websites are really helpful for your business growth... It's a good option for marketing and advertising your stuffs",
+    "coming"
+  );
+});
+functionsList.push(() => {
+  insertNameArrowRightBubble(
+    "You",
+    "Yeah, I know... Actually I  have a business.. And I need a website... That's why I get i  touch with you...",
+    "tick"
+  );
+});
+functionsList.push(() => {
+  insertNameArrowLeftBubble(
+    "Muhammed Rahif",
+    "You are now at the right destination",
+    "coming"
+  );
+});
+functionsList.push(() => {
+  insertNameArrowRightBubble(
+    "You",
+    "Okay, what is your creative skills and experiences ?",
+    "tick"
+  );
+});
+functionsList.push(() => {
+  insertNameArrowLeftBubble(
+    "Muhammed Rahif",
+    "You know what, I am very intresing to know more about creative skills and experiences in programming field espesially in full stack develpoment. Now I am a student so I need to be know more and achieve experinence. To learn more I am researching around internet and solving errors and problems with using stackoverflow, w3school, github, etc. I am very intresting to get more experiences in this field.",
+    "coming"
+  );
+});
+functionsList.push(() => {
+  insertNameArrowRightBubble(
+    "You",
+    "Oh your are student? How old are you ?",
+    "tick"
+  );
+});
+functionsList.push(() => {
+  insertNameArrowLeftBubble(
+    "Muhammed Rahif",
+    "Yeah, I am 16 years old now",
+    "coming"
+  );
+});
+functionsList.push(() => {
+  insertNameArrowRightBubble(
+    "You",
+    "That's cool but what do you know in web development ?",
+    "tick"
+  );
+});
+functionsList.push(() => {
+  insertNameArrowLeftBubble(
+    "Muhammed Rahif",
+    "I know HTML, CSS, JavaScript, React, React-Native, Node.js, mongoDB and etc... And still learning...",
+    "coming"
+  );
+});
+functionsList.push(() => {
+  insertLeftBubble(
+    "That's why I say I am student...!",
+    "coming"
+  );
+});
+functionsList.push(() => {
+  insertNameArrowRightBubble(
+    "You",
+    "That's amazing... Is that true...? I don't believe...",
+    "tick"
+  );
+});
+functionsList.push(() => {
+  insertRightBubble(
+    "A 16 years old boy know react and node js ?",
+    "coming"
+  );
+});
+functionsList.push(() => {
+  insertNameArrowLeftBubble(
+    "Muhammed Rahif",
+    "Yeah... It's all true...for proof check my projects that I have already done...",
+    "coming"
+  );
+});
+functionsList.push(() => {
+  insertLeftBubble(
+    "Also check my github, stackoverflow, fiverr accounts...",
+    "coming"
+  );
+});
+functionsList.push(() => {
+  insertLeftBubble(
+    "links here!",
+    "coming"
+  );
+});
+functionsList.push(() => {
+  insertNameArrowRightBubble(
+    "You",
+    "Wow...",
+    "tick"
+  );
+});
+functionsList.push(() => {
+  insertRightBubble(
+    "How can I contact you?",
+    "tick"
+  );
+});
+functionsList.push(() => {
+  insertNameArrowLeftBubble(
+    "Muhammed Rahif",
+    "To contact me I am giving my address and email down below. And thank you for visiting my website..!",
+    "coming"
+  );
 });
 
+// Run each functions
 function runEachFunctions() {
   var x = 0;
   setInterval(() => {
     if (x === functionsList.length) {
       return;
     } else {
-      console.log(functionsList[x]);
       functionsList[x]();
       x++;
+      var objDiv = document.querySelector(".chat-area");
+      objDiv.scrollTop = objDiv.scrollHeight;
     }
   }, 2500);
 }
